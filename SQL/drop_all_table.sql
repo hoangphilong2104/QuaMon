227caf37cdf6f6f45dfc725ac3174a40a68baa34
Peter <Peter@192.168.0.67>
2023-07-15 14:25:36 +0700

    update

--- SQL/drop_all_table.sql
+++ SQL/drop_all_table.sql
@@ -1,11 +1,18 @@
---ver 1.0
+--ver 1.1
 DECLARE loop_s number(1) := 0;
 begin
+    execute immediate 'drop table '|| 'SALARY_DETAILS';
+    execute immediate 'drop table '|| 'SALARY';
+    execute immediate 'drop table '|| 'POSITION_DETAILS';
+    execute immediate 'drop table '|| 'POSITION';
+    execute immediate 'drop table '|| 'SHIFT';
+    execute immediate 'drop table '|| 'STAGE';
     execute immediate 'drop table '|| 'BILL_DETAILS';
     execute immediate 'drop table '|| 'BILL';
     execute immediate 'drop table '|| 'PRODUCT';
     execute immediate 'drop table '|| 'PRODUCT_CATEGORY';
     execute immediate 'drop table '|| 'SUPPLIER';
     execute immediate 'drop table '|| 'STAFF';
+    execute immediate 'drop table '|| 'MANAGER';
     execute immediate 'drop table '|| 'CUSTOMER';
 end;
